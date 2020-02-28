4ed1160ca48f2a4eb903952bfc3068b5dca69adb
raj vaya <rajkumarvaya@gmail.com>
2020-02-28 11:35:13 +0530

    init commit

---
+++ android/app/src/main/kotlin/codes/raj/solve4bharat_vaani/MainActivity.kt
@@ -0,0 +1,12 @@
+package codes.raj.solve4bharat_vaani
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
