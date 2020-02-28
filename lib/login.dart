09b1be91fc1a83f5f998824385f5138edafb7f89
raj vaya <rajkumarvaya@gmail.com>
2020-02-28 13:17:02 +0530

    Black Logo Added

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:solve4bharat_vaani/SizeConfig.dart';
+import 'style.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -7,8 +8,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  var phone = TextEditingController();
-  var aadhar = TextEditingController();
+
+  var password = TextEditingController();
   var email = TextEditingController();
 
   @override
@@ -16,64 +17,70 @@ class _LoginState extends State<Login> {
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          "Doctor Login",
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.black),
-        elevation: 6,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Card(
-          elevation: 6,
-          child: Column( mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  child: TextField(
-                      controller: email,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: BorderSide(color: Colors.indigo)),
-                          labelText: "Email",
-                          border: OutlineInputBorder(),
-                          hintText: "XYZ@MAIL.COM")),
+//      appBar: AppBar(
+//        title: Text(
+//          "Doctor Login",
+//          style: appbarStyle,
+//        ),
+//        centerTitle: true,
+//        backgroundColor: Colors.white,
+//        iconTheme: IconThemeData(color: Colors.black),
+//        elevation: 6,
+//      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 18, 16, 0),
+          child: Card(
+            elevation: 6,
+            child: Column( mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Center(
+                    child: Image.asset("assets/logo.png" ,height: 100,width: 100,),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  child: TextField(
-                      controller: aadhar,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: BorderSide(color: Colors.indigo)),
-                          labelText: "Password",
-                          border: OutlineInputBorder(),
-                          hintText: "XXXX XXXX")),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    child: TextField(
+                        controller: email,
+                        decoration: InputDecoration(
+                            enabledBorder: outlineInputBorder,
+                            focusedBorder: outlineInputBorder,
+                            labelText: "Email",
+                            labelStyle: inputlableStyle,
+                            border: OutlineInputBorder(),
+                            hintText: "XYZ@MAIL.COM")),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: FloatingActionButton.extended(
-                    onPressed: () async {
-                      _showDialog();
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    child: TextField(
+                        controller: password,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            enabledBorder: outlineInputBorder,
+                             focusedBorder: outlineInputBorder,labelStyle: inputlableStyle,
+                            labelText: "Password",
+                            border: OutlineInputBorder(),
+                            hintText: "XXXX XXXX")),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FloatingActionButton.extended(
+                      onPressed: () async {
+                        _showDialog();
 
-                    },
-                    icon: Icon(Icons.verified_user),
-                    label: Text("Login"),
-                    backgroundColor: Colors.indigo[300]),
-              ),
-            ],
+                      },
+                      icon: Icon(Icons.verified_user),
+                      label: Text("Login"),
+                      backgroundColor: Colors.indigo[300]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/style.dart
+++ lib/style.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle appbarStyle =
+    GoogleFonts.pTSans(fontSize: 30, color: Colors.grey[700]);
+
+TextStyle inputlableStyle =
+    GoogleFonts.pTSans(fontSize: 18, color: Colors.indigo);
+
+OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: BorderSide(color: Colors.indigo));
