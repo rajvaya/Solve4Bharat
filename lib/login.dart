6de05207209153a03490c5fe1455b3ca7459c526
raj vaya <rajkumarvaya@gmail.com>
2020-02-28 12:42:58 +0530

    Login Basic UI

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solve4bharat_vaani/SizeConfig.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -6,8 +7,108 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  var phone = TextEditingController();
+  var aadhar = TextEditingController();
+  var email = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "Doctor Login",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 6,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Card(
+          elevation: 6,
+          child: Column( mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  child: TextField(
+                      controller: email,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide(color: Colors.indigo)),
+                          labelText: "Email",
+                          border: OutlineInputBorder(),
+                          hintText: "XYZ@MAIL.COM")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  child: TextField(
+                      controller: aadhar,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide(color: Colors.indigo)),
+                          labelText: "Password",
+                          border: OutlineInputBorder(),
+                          hintText: "XXXX XXXX")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FloatingActionButton.extended(
+                    onPressed: () async {
+                      _showDialog();
+
+                    },
+                    icon: Icon(Icons.verified_user),
+                    label: Text("Login"),
+                    backgroundColor: Colors.indigo[300]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Please Wait"),
+          content: Wrap(
+            children: <Widget>[
+              Center(
+                child: CircularProgressIndicator(
+                  valueColor: new AlwaysStoppedAnimation<Color>(
+                    Colors.indigo[300],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+
+
+
+
 }
+
+
